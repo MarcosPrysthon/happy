96ecfd0685762e15c4f52b7dc2ad3753d91dee78
MarcosPrysthon <mvpn@cin.ufpe.br>
2020-10-15 21:18:47 -0300

    criando arquivo de rotas separadamente e controlador de orfanatos

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+    async create(req: Request, res: Response) {
+        const { 
+            name,
+            latitude,
+            longitude,
+            about, 
+            instructions,
+            opening_hours,
+            open_on_weekends
+         } = req.body;
+    
+        const orphanagesRepo = getRepository(Orphanage); 
+    
+        const orphanage = orphanagesRepo.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        });
+    
+        await orphanagesRepo.save(orphanage);
+    
+        return res.status(201).json(orphanage); 
+    },
+
+    async index(req: Request, res: Response){
+        const orphanagesRepo = getRepository(Orphanage); 
+
+        const orphanages = await orphanagesRepo.find();
+
+        return res.json(orphanages);
+    }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,11 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
 
+import routes from './routes';
+
 const app = express();
+
 app.use(express.json());
-
-app.post('/orphanages', async (req, res) => {
-    const { 
-        name,
-        latitude,
-        longitude,
-        about, 
-        instructions,
-        opening_hours,
-        open_on_weekends
-     } = req.body;
-
-    const orphanagesRepo = getRepository(Orphanage); 
-
-    const orphanage = orphanagesRepo.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepo.save(orphanage);
-
-    return res.json({ message: "hello" }); 
-});
+app.use(routes);
 
 app.listen(3333);
