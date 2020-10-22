6ab8b337de1400692fae358686a5485481c42c92
MarcosPrysthon <mvpn@cin.ufpe.br>
2020-10-22 02:33:35 -0300

    criando a validacao dos dados com Yup

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,7 @@ import { getRepository } from 'typeorm';
 
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
     async create(req: Request, res: Response) {
@@ -21,19 +22,38 @@ export default {
         const images = reqImages.map(image => {
             return { path: image.filename }
         })
-    
-        const orphanagesRepo = getRepository(Orphanage); 
-    
-        const orphanage = orphanagesRepo.create({
+
+        const data = {
             name,
             latitude,
             longitude,
-            about,
+            about, 
             instructions,
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const scheema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(340),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required()
+            }))
         });
+
+        await scheema.validate(data, {
+            abortEarly: false
+        });
+    
+        const orphanagesRepo = getRepository(Orphanage); 
+    
+        const orphanage = orphanagesRepo.create(data);
     
         await orphanagesRepo.save(orphanage);
     
