fb25612d8bd44049d2f83273f0416e73afbb43a9
MarcosPrysthon <mvpn@cin.ufpe.br>
2020-10-29 03:27:52 -0300

    finalizando a pagina de mapa

--- web/src/Pages/OrphanagesMap.tsx
+++ web/src/Pages/OrphanagesMap.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi'
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
 import '../styles/pages/orphanages-map.css';
 
-import mapMarkerImg from '../images/map-marker.svg';
+import mapMarkerImg from '../images/logo.svg';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [80, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [130, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -32,6 +40,22 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                <Marker 
+                 position={[-8.1002496,-34.8955189]}
+                 icon={mapIcon}
+                >
+                    <Popup 
+                     closeButton={false}
+                     minWidth={240}
+                     maxWidth={240}
+                     className="map-popup">
+                        Lar doce Lar
+                        <Link to="">
+                            <FiArrowRight size={20} color="#FFF"/>
+                        </Link>
+                    </Popup>
+                </ Marker>
             </Map>
 
             <Link to="" className="create-orphanage">
